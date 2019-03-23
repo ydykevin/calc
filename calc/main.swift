949ff5212fd86da563daaf729dcb26a7ad0332a0
ydykevin <641877858@qq.com>
2019-03-23 21:59:49 +1100

    All done except invalid input

--- calc/main.swift
+++ calc/main.swift
@@ -1,14 +1,8 @@
-//
-//  main.swift
-//  calc
-//
-//  Created by Jesse Clark on 12/3/18.
-//  Copyright © 2018 UTS. All rights reserved.
-//
-
 import Foundation
 
 var args = ProcessInfo.processInfo.arguments
-args.removeFirst() // remove the name of the program
+args.removeFirst()
 
-print(Int(args[0])!)
+var cal = Calc()
+cal.calculate(args: &args)
+
